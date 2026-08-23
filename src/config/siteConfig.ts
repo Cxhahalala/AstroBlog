322beef7ad2edc16e04df85db3f403aed5951d68
cx <tommorrow_better@163.com>
2026-08-23 18:57:15 +0800

    feat: configure site title to xinlog - Today Better and update browser favicon

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -5,7 +5,7 @@ const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 
 export const siteConfig: SiteConfig = {
   title: "xinlog",
-  subtitle: "One demo website",
+  subtitle: "Today Better",
   siteURL: "https://xinlog.pages.dev/", // 请替换为你的站点URL，以斜杠结尾
   siteStartDate: "2025-01-01", // 站点开始运行日期，用于站点统计组件计算运行天数
   timeZone: "Asia/Shanghai", // 文章日期使用的 IANA 时区，可改为 Asia/Tokyo、Europe/Berlin 等
@@ -203,12 +203,9 @@ export const siteConfig: SiteConfig = {
   showCoverInContent: true, // 在文章内容页显示文章封面
   generateOgImages: false, // 启用生成OpenGraph图片功能,注意开启后要渲染很长时间，不建议本地调试的时候开启
   favicon: [
-    // 留空以使用默认 favicon
-    // {
-    //   src: '/favicon/icon.png',    // 图标文件路径
-    //   theme: 'light',              // 可选，指定主题 'light' | 'dark'
-    //   sizes: '32x32',              // 可选，图标大小
-    // }
+    {
+      src: "/Background/头像1.png",
+    },
   ],
 
   showLastModified: true, // 控制"上次编辑"卡片显示的开关
